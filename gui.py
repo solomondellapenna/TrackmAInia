e1f4c28b8cd3d1fe268211dcdda0b41da8de0779
RyanBHuynh <ryanhuynh@mu-586068.dhcp.missouri.edu>
2023-03-13 14:37:56 -0500

    trying to get the terminal windows to start

--- gui.py
+++ gui.py
@@ -1,10 +1,12 @@
 import wx
 import os
+import subprocess
 
 # Global variables
 
 MODEL_PATH = "../models"
 current_model = ""
+terminals = []
 
 class MyFrame(wx.Frame):
     def __init__(self):
@@ -22,24 +24,38 @@ class MyFrame(wx.Frame):
         current_model = model_selection_combo_box.GetValue()
 
         # Create a button to train the model
-        button = wx.Button(panel, label="Train model")
-        button.Bind(wx.EVT_BUTTON, self.on_button_click)
+        button = wx.Button(panel, label="Test model")
+        button.Bind(wx.EVT_BUTTON, self.on_test_model_click)
 
         # Create a sizer to manage the layout of child widgets
         sizer = wx.BoxSizer(wx.VERTICAL)
 
-        sizer.Add(model_selection_text, 0, wx.CENTER | wx.ALL, 20) # Add the model selection text to the sizer
-        sizer.Add(button, 0, wx.CENTER | wx.ALL, 20) # Add the button to the sizer
-        sizer.Add(model_selection_combo_box, 0, wx.CENTER | wx.ALL, 20) # Add the model selection combo box to the sizer
+        sizer.Add(model_selection_text, 0, wx.CENTER | wx.ALL, 5) # Add the model selection text to the sizer
+        sizer.Add(model_selection_combo_box, 0, wx.CENTER | wx.ALL, 5) # Add the model selection combo box to the sizer
+        sizer.Add(button, 0, wx.CENTER | wx.ALL, 5) # Add the button to the sizer
 
         panel.SetSizer(sizer)
 
-    def on_button_click(self, event):
+    def on_test_model_click(self, event):
         print("Button clicked!")
 
+        # Start up terminals to run tmrl
+        subprocess.Popen(['open', '-a', 'Terminal.app', 'ipconfig'])
+        # server_term = subprocess.Popen(['open', '-a', 'Terminal.app', 'man man'], stdin=subprocess.PIPE)
+        # trainer_term = subprocess.Popen(['open', '-a', 'Terminal.app', 'man man'], stdin=subprocess.PIPE)
+        # worker_term = subprocess.Popen(['open', '-a', 'Terminal.app', 'ifconfig'], stdin=subprocess.PIPE)
+        # server_term = subprocess.Popen(['open', '-e', 'python -m tmrl --server'], stdin=subprocess.PIPE)
+        # trainer_term = subprocess.Popen(['open', '-e', 'python -m tmrl --trainer'], stdin=subprocess.PIPE)
+        # worker_term = subprocess.Popen(['open', '-e', 'python -m tmrl --worker'], stdin=subprocess.PIPE)
+
+        # Add the terminals to the terminals list
+        # terminals.append(server_term)
+        # terminals.append(trainer_term)
+        # terminals.append(worker_term)
+
     def on_model_selection(self, event):
-        print("Model selected!")
         current_model = event.GetString()
+        print("current_model =", current_model)
 
 if __name__ == '__main__':
     app = wx.App()
